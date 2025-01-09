5cea01f4cf5e49908beebbead7725d9ff17a91d2
Mohitsingh-2006 <ee24btech11041@iith.ac.in>
2025-01-09 09:57:51 +0530

    Removed Q1pre

--- Presentation1/codes/Finite.c
+++ Presentation1/codes/Finite.c
@@ -2,12 +2,10 @@
 #include <math.h>
 
 float h = 0.001;
-
 // Derivative function
 float derivative(float y, float x) {
     return 1/(x*(x*x-1));
 }
-
 // Solution function
 void solution(float *x, float *y, int n) {
     for (int i = 1; i <= n; i++) {
